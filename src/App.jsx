aebe77c4181de80c82fc37684d5058816375aa43
Hannah Li <46640853+Hannahli6@users.noreply.github.com>
2022-02-05 06:36:10 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h1>Zero Waste Receipe App</h1>
+        <div classname="expire-food-container">
+          <div>
+            <span></span>
+          </div>
+        </div>
+
+      </div>
+  );
+}
+
+export default App;
+
+//WOOOOOOOO
